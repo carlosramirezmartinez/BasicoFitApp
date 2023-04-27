6e352649347edea75603b06b17c1d19c59ee8e47
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-27 19:52:28 +0200

    Update BasicoFitDB.sql

--- BasicoFitDB.sql
+++ BasicoFitDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 27-04-2023 a las 17:04:34
+-- Tiempo de generación: 27-04-2023 a las 17:51:39
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
@@ -37,20 +37,20 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `email` varchar(255) NOT NULL,
   `ciudad` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`,`DNI`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
 INSERT INTO `clientes` (`ID`, `DNI`, `nombre`, `apellidos`, `fecha_nacimiento`, `email`, `ciudad`) VALUES
-(1, '55555555R', 'Carlos', 'Ramirez Martinez', '1999-08-08', 'carlosramirezmartinez01@gmail.com', 'Madrid');
-(2, '55555555K', 'Benito', 'Ramirez Perez', '1993-02-01', 'benito3@gmail.com', 'Murcia');
-(3, '55555555L', 'Laura', 'Pons Pinto', '1988-07-08', 'laurita01@gmail.com', 'Sevilla');
-(4, '55555555M', 'Emily', 'Almeida', '2000-12-08', 'lamili0000@gmail.com', 'Madrid');
-(5, '55555567Y', 'Eduardo', 'Criado Martinez', '1986-08-12', 'educr7@gmail.com', 'Madrid');
-(6, '66666664K', 'Enrique', 'Pastor Martinez', '1950-02-02', 'concejal@gmail.com', 'Malaga');
-(7, '12345677J', 'Santiago', 'Abascal Martinez', '1978-08-08', 'santivox@gmail.com', 'Madrid');
+(1, '55555555R', 'Carlos', 'Ramirez Martinez', '1999-08-08', 'carlosramirezmartinez01@gmail.com', 'Madrid'),
+(2, '55555555K', 'Benito', 'Ramirez Perez', '1993-02-01', 'benito3@gmail.com', 'Murcia'),
+(3, '55555555L', 'Laura', 'Pons Pinto', '1988-07-08', 'laurita01@gmail.com', 'Sevilla'),
+(4, '55555555M', 'Emily', 'Almeida', '2000-12-08', 'lamili0000@gmail.com', 'Madrid'),
+(5, '55555567Y', 'Eduardo', 'Criado Martinez', '1986-08-12', 'educr7@gmail.com', 'Madrid'),
+(6, '66666664K', 'Enrique', 'Pastor Martinez', '1950-02-02', 'concejal@gmail.com', 'Malaga'),
+(7, '12345677J', 'Santiago', 'Abascal Martinez', '1978-08-08', 'santivox@gmail.com', 'Madrid'),
 (8, '88888888U', 'Pedro', 'Sanchez Sanchez', '1980-08-08', 'pedropsoe@gmail.com', 'Madrid');
 COMMIT;
 
