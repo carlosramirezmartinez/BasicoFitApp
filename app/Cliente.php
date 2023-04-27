751f42a3fd510819ce9e617719de3ef344ec53e7
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-27 21:14:04 +0200

    creacion mvc + objeto Cliente

---
+++ app/Cliente.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cliente
+{
+   private $id;
+   private $dni;
+   private $nombre;
+   private $apellidos;
+   private $fecha_nacimiento;
+   private $email;
+   private $ciudad;
+
+   
+   // Getter con método mágico
+   public function __get($atributo){
+       $class = get_class($this);
+       if(property_exists($class, $atributo)) {
+           return $this->$atributo;
+       }
+   }
+   
+   // Set con método mágico
+   public function __set($atributo,$valor){
+       $class = get_class($this);
+       if(property_exists($class, $atributo)) {
+           $this->$atributo = $valor;
+       }
+   }
+   
+}
+
