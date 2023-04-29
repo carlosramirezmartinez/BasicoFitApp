f870b714c586550f4c42beacb521b7c9c8e45994
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-29 13:37:37 +0200

    Enrutamiento + plantillas + CSS y JS (solo base)

--- index.php
+++ index.php
@@ -5,3 +5,11 @@ include_once 'app/modeloCliente.php';
 include_once 'app/Cliente.php';
 
 echo "index ok";
+
+$rutasClientes = [
+    "Inicio"      => "ctlClienteInicio",
+    "Alta"        => "ctlClienteAlta",
+    "Detalles"    => "ctlClienteDetalles",
+    "Modificar"   => "ctlClienteModificar",
+    "Borrar"      => "ctlClienteBorrar"
+];
