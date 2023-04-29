e864caaf855838035188ca331134a9c16d3317ab
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-29 18:02:52 +0200

    Update modeloCliente.php

--- app/modeloCliente.php
+++ app/modeloCliente.php
@@ -4,10 +4,10 @@ include_once 'config.php';
 include_once 'Cliente.php';
 
 //Consultas SQL, pruebas previas con phpmyadmin
-class ModeloUserDB{
+class ModeloClienteDB{
 
     private static $dbh = null;
-    private static $consulta_cliente = "Select * from clientes where ID = ?, DNI = ?";
+    private static $consulta_cliente = "Select * from clientes where ID = ?";
 
     //Alta, modificar y borrar
     private static $insert_cliente   = "Insert into clientes (DNI, nombre, apellidos, fecha_nacimiento, email, ciudad)" .
@@ -16,6 +16,25 @@ class ModeloUserDB{
     private static $delete_cliente  = "Delete from clientes where ID = ?";
 
 
+
+
+
+//Inicio conexion con la BD     
+public static function init(){  
+    if (self::$dbh == null){
+        try {
+            // Cambiar  los valores de las constantes en config.php
+            $dsn = "mysql:host=".DBSERVER.";dbname=".DBNAME.";charset=utf8";
+            self::$dbh = new PDO($dsn,DBUSER,DBPASSWORD);
+            // Si se produce un error se genera una excepción;
+            self::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e){
+            echo "Error de conexión ".$e->getMessage();
+            exit();
+        }
+        
+    }
+    
 }
 
 /**
@@ -24,3 +43,20 @@ class ModeloUserDB{
  * GetOne, GetAll, Clientes borrado, alta, modify, etc
  * 
  */
+
+// Tabla de objetos con los cliente
+public static function GetAll ():array{
+    // Genero los datos para la vista de index
+    
+    $stmt = self::$dbh->query("select * from clientes");
+    
+    $tclientes = [];
+    $stmt->setFetchMode(PDO::FETCH_CLASS, 'Cliente');
+    while ( $cliente = $stmt->fetch()){
+        $tclientes[] = $cliente;       
+    }
+    return $tclientes;
+}
+
+
+}//Fin clase
