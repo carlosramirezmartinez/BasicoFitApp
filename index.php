bea6069dfd12869c9c9d951447f4a6bfbc720712
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-29 16:39:55 +0200

    maquetado html y base de css

--- index.php
+++ index.php
@@ -4,11 +4,39 @@ include_once 'app/config.php';
 include_once 'app/modeloCliente.php'; 
 include_once 'app/Cliente.php';
 
-echo "index ok";
+// Inicializo el modelo 
+ModeloPeliDB::Init();
+
+//echo "index ok";
 
 $rutasClientes = [
     "Alta"        => "ctlClienteAlta",
     "Detalles"    => "ctlClienteDetalles",
     "Modificar"   => "ctlClienteModificar",
-    "Borrar"      => "ctlClienteBorrar"
+    "Borrar"      => "ctlClienteBorrar",
+    "VerClientes"    => "ctlClienteVerClientes",
 ];
+
+if (isset($_GET['orden'])){
+    // La orden tiene una funcion asociada 
+    if ( isset ($rutasClientes[$_GET['orden']]) ){
+        $procRuta =  $rutasClientes[$_GET['orden']];
+    }
+    else {
+        // Error no existe función para la ruta
+        header('Status: 404 Not Found');
+        echo '<html><body><h1>Error 404: No existe la ruta <i>' .
+            $_GET['ctl'] .
+            '</p></body></html>';
+            exit;
+    }
+}
+else {
+    $procRuta = "ctlClienteVerClientes";
+}
+
+
+// Llamo a la función seleccionada
+$procRuta();
+
+
